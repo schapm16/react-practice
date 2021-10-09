9784c17977be788934edfb338e3882bfb0e11430
Stephen Chapman <Schapm16@gmail.com>
2021-10-09 19:44:31 -0400

    Context: Express Server - Install ExpressJS dependency and set up server boilerplate

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log('Hello World')
+const express = require('express');
+const app = express();
+const PORT = 3001;
+
+app.listen(PORT, () => {
+  console.log(`Example app listening at http://localhost:${PORT}`)
+})
