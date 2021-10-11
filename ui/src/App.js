267702d364e7925adde8d07347dce0277559b751
Stephen Chapman <Schapm16@gmail.com>
2021-10-11 13:30:19 -0400

    Context: UI Work - Create basic React routes

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,10 +1,17 @@
-import './App.css';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { Home, OrganizationDetail } from './pages/'
 
 function App() {
   return (
-    <div className="App">
-    </div>
-  );
+    <BrowserRouter>
+      <Switch>
+        <Route path='/organization-detail' component= { OrganizationDetail }/>
+        <Route path='/'>
+          <Home />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  )
 }
 
 export default App;
---
+++ ui/src/pages/index.js
@@ -0,0 +1,7 @@
+import Home from './Home';
+import OrganizationDetail from './OrganizationDetail';
+
+export {
+  Home,
+  OrganizationDetail
+}
