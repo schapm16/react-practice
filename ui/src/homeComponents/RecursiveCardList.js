ebeacf2f5c16719e31a2bc9da56f223fca08355f
Stephen Chapman <Schapm16@gmail.com>
2021-10-12 12:16:48 -0400

    Context: UI Work - Add addition data to cards on `Home`

--- ui/src/homeComponents/RecursiveCardList.js
+++ ui/src/homeComponents/RecursiveCardList.js
@@ -4,7 +4,7 @@ import './RecursiveCardList.css';
 function SingleCardList({ data }) {
   return (
     <ul className="card-list">
-      {data.map(({avatar_url, description, id, login}) => {
+      {data.map(({avatar_url, description, id, login, url_data: { followers, public_repos, public_gists }}) => {
         return (
           <li className="card" key={id}>
             <div className="card-header">
@@ -12,6 +12,11 @@ function SingleCardList({ data }) {
               <h2 className="card-title">{login}</h2>
             </div>
             <div className="card-body">
+              <p className="card-body-stat">Followers: <span className="card-body-stat--bold">{followers}</span></p>
+              <p className="card-body-stat">Public Repos: <span className="card-body-stat--bold">{public_repos}</span></p>
+              <p className="card-body-stat">Public Gists: <span className="card-body-stat--bold">{public_gists}</span></p>
+            </div>
+            <div className="card-footer">
               <p>{description}</p>
             </div>
           </li>
