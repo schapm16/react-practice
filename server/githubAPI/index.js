524bb0444a5ea42600e8aaf149f905e29f8baa45
Stephen Chapman <Schapm16@gmail.com>
2021-10-12 11:36:39 -0400

    Context: Express Server - Combine data from multiple GitHub endpoints to enrich data when receiving   requests to `/api/get-organizations` from Client

--- server/githubAPI/index.js
+++ server/githubAPI/index.js
@@ -6,7 +6,14 @@ axios.defaults.headers = {
   'accept': 'application/vnd.github.v3+json',
 }
 
-function getOrganizations(query = {}) {
+
+/**
+ * Submits GET to GitHub /organizations
+ *
+ * @param {*} [query={}]
+ * @return {Promise<Array>} 
+ */
+function getOrganizationsList(query = {}) {
   const search = {
     per_page: 10,
     since: parseInt(query.lastId, 10) + 1 || 1000,
@@ -16,6 +23,40 @@ function getOrganizations(query = {}) {
 
   return axios.get(endpoint)
     .then(response => response.data)
+}
+
+
+/**
+ * Submits GET to GitHub /orgs/{org}
+ *
+ * @param {string} organizationUrl
+ * @return {Promise<Object>}
+ */
+function getOrganizationOrg(organizationUrl) {
+  if (!organizationUrl) return Promise.resolve({});
+
+  return axios.get(organizationUrl)
+    .then(response => response.data)
+}
+
+
+/**
+ * Combines GET /organizations and GET /orgs/{org} before returning
+ * to client Request
+ *
+ * @param {*} [query={}]
+ * @return {Promise<Array>} 
+ */
+async function getOrganizations(query = {}) {
+  const organizationList = await getOrganizationsList(query);
+  const urlDataPromiseArray = organizationList.map(async (organization) => {
+    let urlDataPromise = getOrganizationOrg(organization.url);
+    organization.url_data = await urlDataPromise;
+    return urlDataPromise; 
+  })
+
+  return Promise.all(urlDataPromiseArray)
+    .then(() => organizationList);
 }
 
 module.exports = {
