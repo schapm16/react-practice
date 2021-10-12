628baed46f274c9911712419efe7bed2d15785e5
Stephen Chapman <Schapm16@gmail.com>
2021-10-12 10:46:02 -0400

    Context: UI Work - Implement call for organization data on `Home`

--- ui/src/pages/Home.js
+++ ui/src/pages/Home.js
@@ -1,9 +1,17 @@
 import React, { useEffect, useState } from 'react';
 import './Home.css';
 import { RecursiveCardList } from '../homeComponents';
+import { getOrganizations } from '../requestData';
 
 export default function Home() {
   const [ organizations, setOrganizations ] = useState([]);
+  
+  useEffect(() => {
+    getOrganizations()
+      .then(data => {
+        setOrganizations(data);
+      })
+  }, [])
     
   return (
     <section className="home">
