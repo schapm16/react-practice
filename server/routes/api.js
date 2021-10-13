eb72f73a2af73efc5ebbbdbfaba8a8726766592e
Stephen Chapman <Schapm16@gmail.com>
2021-10-13 17:12:13 -0400

    Context: Express Server Work - Create `/get-organization-detail` endpoint on my server

--- server/routes/api.js
+++ server/routes/api.js
@@ -15,4 +15,11 @@ router.get('/get-organizations', (request, response) => {
     .then(data => response.json(data))
 })
 
+router.get('/get-organization-detail', (request, response) => {
+  const { org } = request.query;
+
+  getOrganizationDetail(org)
+    .then(data => response.json(data));
+})
+
 module.exports = router;
