41775f79783ce0ff3c691c02bdbee502da02e632
Stephen Chapman <Schapm16@gmail.com>
2021-10-13 18:00:05 -0400

    Context: Express Server Work - Set up for potential production deploy

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const app = express();
 const PORT = 3001;
@@ -5,6 +6,14 @@ const PORT = 3001;
 const { apiRouter } = require('./server/routes/');
 
 app.use('/api', apiRouter);
+
+if (process.env.NODE_ENV === 'production') {
+  const uiRoot = path.join(__dirname, 'ui', 'build');
+  app.use(express.static(uiRoot));
+  app.get('/*', (request, response) => {
+    response.sendFile('index.html', {root: uiRoot});
+  })
+}
 
 
 app.listen(PORT, () => {
