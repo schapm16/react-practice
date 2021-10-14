2812a75a7cf0451660fe09d8d357f26ceee2e152
Stephen Chapman <Schapm16@gmail.com>
2021-10-14 00:37:41 -0400

    Context: UI Work - Start work on `organizationDetail` page

--- ui/src/pages/OrganizationDetail.js
+++ ui/src/pages/OrganizationDetail.js
@@ -1,8 +1,38 @@
-export default function Home(props) {
+import React, { useEffect, useState } from 'react';
+import './OrganizationDetail.css';
+import { getOrganizationDetail } from '../requestData';
+
+export default function OrganizationDetail({ location }) {
+
+  let [ organizationDetail, setOrganizationDetail ] = useState(null);
+
+  useEffect(() => {
+    getOrganizationDetail('railslove')
+      .then(data => setOrganizationDetail(data));
+  }, [])
+
+  if (!organizationDetail) return <div></div>;
+
   return (
-    <div>
-    <h1>Hello World from Organization Detail page</h1>
-    <p>{props.location.hash}</p>
+    <div className='organization-detail grid-container'>
+      <section id="members" className="grid-item">
+        <h2>Members</h2>
+        {organizationDetail.members_data && organizationDetail.members_data.map(member => {
+          return (
+            <div className="member-container">
+              <img className="member-avatar" alt={`${member.login} avatar`} src={member.avatar_url}/>
+              <p className="member-name">{member.login}</p>
+              <a 
+                className="createOverlay member-link"
+                href={member.html_url}
+                aria-label={`Link to ${member.login} profile on GitHub`}>
+              </a>
+            </div>
+          )
+        })}
+      </section>
+      <section id="repos" className="grid-item"></section>
+      <section id="gists" className="grid-item"></section>
     </div>
   )
 }
